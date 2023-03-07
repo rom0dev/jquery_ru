44a09a0612eedbf2acce826a9fc24fd9a5d5c6fb
Roman Milovsky <romdev@vivaldi.net>
2023-03-07 11:45:03 +0300

    Add my stat collector

--- push.sh
+++ push.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ssh-agent sh -c 'ssh-add ~/.ssh/digitalocean_romsky; git push git@github.com:rom0dev/jquery_ru.git'
+#ssh-agent sh -c 'ssh-add ~/.ssh/digitalocean_romsky; git push git@github.com:rom0dev/jquery_ru.git'
+ssh-agent sh -c 'ssh-add ~/.ssh/rom0dev; git push git@github.com:rom0dev/jquery_ru.git'
